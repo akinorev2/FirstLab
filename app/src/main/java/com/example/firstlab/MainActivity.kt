01e213c75af1a8b157413e70c045ddaa96afe72d
akinorev2 <miroshnichenko2506@icloud.com>
2023-09-28 14:10:17 +0300

    Added button ChangeColor

--- app/src/main/java/com/example/firstlab/MainActivity.kt
+++ app/src/main/java/com/example/firstlab/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.example.firstlab
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button: Button
+    private lateinit var buttonChangeText: Button
+    private lateinit var buttonChangeColor: Button
     private lateinit var textView: TextView
+
+    private var textColorChanged = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
+        // Initialize views
+        buttonChangeText = findViewById(R.id.buttonChangeText)
+        buttonChangeColor = findViewById(R.id.buttonChangeColor)
         textView = findViewById(R.id.textView)
 
-
-        button.setOnClickListener {
-
+        // Set a click listener for the "Change Text" Button
+        buttonChangeText.setOnClickListener {
             textView.text = "Text Changed!"
+            // Reset text color to default
+            textView.setTextColor(Color.BLACK)
+            textColorChanged = false
+        }
+
+        // Set a click listener for the "Change Color" Button
+        buttonChangeColor.setOnClickListener {
+            if (!textColorChanged) {
+                textView.setTextColor(Color.RED)
+            } else {
+                textView.setTextColor(Color.BLUE)
+            }
+            textColorChanged = !textColorChanged
         }
     }
 }

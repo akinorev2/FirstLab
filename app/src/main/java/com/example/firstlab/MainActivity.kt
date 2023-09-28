dd5df41c6d8e4d7445da03326371678318233f1e
akinorev2 <miroshnichenko2506@icloud.com>
2023-09-28 14:02:17 +0300

    Added button ChangeText

--- app/src/main/java/com/example/firstlab/MainActivity.kt
+++ app/src/main/java/com/example/firstlab/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.firstlab
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+
+        button.setOnClickListener {
+
+            textView.text = "Text Changed!"
+        }
     }
 }

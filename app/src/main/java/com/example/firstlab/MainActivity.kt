7f255658178613a8bfab4bdc1881ee6e18054286
akinorev2 <miroshnichenko2506@icloud.com>
2023-09-28 14:13:57 +0300

    Revert "Removed Redundant ChangeColor Button"
    This reverts commit 1f47a03e60450420b6843c8cb08956033a3d0a4c.

--- app/src/main/java/com/example/firstlab/MainActivity.kt
+++ app/src/main/java/com/example/firstlab/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.firstlab
 import android.graphics.Color
 import android.os.Bundle
-
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var buttonChangeText: Button
-
+    private lateinit var buttonChangeColor: Button
     private lateinit var textView: TextView
 
     private var textColorChanged = false
@@ -18,19 +18,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // Initialize views
         buttonChangeText = findViewById(R.id.buttonChangeText)
-
+        buttonChangeColor = findViewById(R.id.buttonChangeColor)
         textView = findViewById(R.id.textView)
 
+        // Set a click listener for the "Change Text" Button
         buttonChangeText.setOnClickListener {
             textView.text = "Text Changed!"
-
+            // Reset text color to default
             textView.setTextColor(Color.BLACK)
-
+            textColorChanged = false
         }
 
-
-
+        // Set a click listener for the "Change Color" Button
+        buttonChangeColor.setOnClickListener {
+            if (!textColorChanged) {
+                textView.setTextColor(Color.RED)
+            } else {
+                textView.setTextColor(Color.BLUE)
+            }
+            textColorChanged = !textColorChanged
+        }
     }
 }
